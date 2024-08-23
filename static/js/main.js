7d54fd59166ac030483c83cd8c1b3c7c92cfd617
Taz <43081043+Tazhossain@users.noreply.github.com>
2024-08-23 23:59:52 +0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,29 +2,42 @@ const socket = io();
 const chatbox = document.getElementById('chatbox');
 const chatForm = document.getElementById('chat-form');
 const messageInput = document.getElementById('message');
+const notificationSound = new Audio('/static/sounds/notification.mp3');
 let nightMode = false;
 let notificationsEnabled = false;
 let soundEnabled = false;
 
-socket.on('message', function(data) {
+// Append message to chatbox with timestamp
+function appendMessage(data) {
     const div = document.createElement('div');
-    div.textContent = data.msg;
+    div.textContent = `[${data.timestamp}] ${data.msg}`;
     chatbox.appendChild(div);
     chatbox.scrollTop = chatbox.scrollHeight;
+}
 
-    if (notificationsEnabled) {
-        new Notification('New message', { body: data.msg });
+socket.on('message', function(data) {
+    appendMessage(data);
+
+    if (notificationsEnabled && Notification.permission === 'granted') {
+        new Notification('New message', {
+            body: data.msg,
+            timestamp: data.timestamp
+        });
     }
 
     if (soundEnabled) {
-        // Play notification sound
-        const audio = new Audio('/static/sounds/notification.mp3');
-        audio.play();
+        notificationSound.play();
     }
 });
 
 socket.on('update_users', function(users) {
-    // Update the user list (not shown in UI yet)
+    const userList = document.getElementById('user-list');
+    userList.innerHTML = '';
+    users.forEach(user => {
+        const userDiv = document.createElement('div');
+        userDiv.textContent = user.nickname;
+        userList.appendChild(userDiv);
+    });
 });
 
 chatForm.addEventListener('submit', function(event) {
@@ -46,14 +59,22 @@ function clearChat() {
 }
 
 function toggleNotifications() {
-    notificationsEnabled = !notificationsEnabled;
-    if (notificationsEnabled && Notification.permission !== 'granted') {
-        Notification.requestPermission();
+    if (!notificationsEnabled && Notification.permission !== 'granted') {
+        Notification.requestPermission().then(permission => {
+            if (permission === 'granted') {
+                notificationsEnabled = true;
+                alert('Notifications enabled.');
+            }
+        });
+    } else {
+        notificationsEnabled = !notificationsEnabled;
+        alert(`Notifications ${notificationsEnabled ? 'enabled' : 'disabled'}.`);
     }
 }
 
 function toggleSound() {
     soundEnabled = !soundEnabled;
+    alert(`Sound ${soundEnabled ? 'enabled' : 'disabled'}.`);
 }
 
 function toggleMenu() {
@@ -70,9 +91,6 @@ socket.on('clear_chat', function() {
 socket.on('update_chat', function(messages) {
     chatbox.innerHTML = '';
     messages.forEach(function(message) {
-        const div = document.createElement('div');
-        div.textContent = `${message.nickname}: ${message.msg}`;
-        chatbox.appendChild(div);
+        appendMessage(message);
     });
-    chatbox.scrollTop = chatbox.scrollHeight;
 });
